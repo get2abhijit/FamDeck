2d3d2f9e9c22af9b710fbfa4daa0cc43da2d89c8
Abhijit Deshpande <abhijit.deshpande@assyst.de>
2026-07-01 00:27:51 +0200

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,8 @@ export default defineConfig({
         theme_color: '#0f172a',
         background_color: '#0f172a',
         display: 'standalone',
-        start_url: '/',
+        // start_url: '/',
+        start_url: '/FamDeck/',
         icons: [
           {
             src: '/pwa-192.svg',
